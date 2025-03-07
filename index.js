0aeeef5d649c5ecb49814fad0d061ce4c47930b8
zeitunmohamed <zeitunmohamed@gmail.com>
2025-03-07 21:26:02 +0300

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
-// Code your solutions in this file
+
+function writeCards(names, event) {
+  return names.map(name => `Thank you, ${name}, for the wonderful ${event} gift!`);
+}
+
+describe('writeCards()', () => {
+  it('returns an array of thank you messages for each name provided to the function', () => {
+    const result = writeCards(["Guadalupe", "Ollie", "Aki"], "surprise");
+    
+    let messages=([]);
+  });
+});
+
+
+  
+function countDown(number) {
+    while (number >= 0) {
+      console.log(number);
+      number--;
+    }
+  }
+  
+  module.exports = { writeCards, countDown };
+  
